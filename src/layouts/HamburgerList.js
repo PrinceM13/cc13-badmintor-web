be43826c554c8414d19b33f5f2c6c05ec7b7cdbe
PrinceM <parames.pasitprachaja@gmail.com>
2023-02-07 12:29:19 +0700

    fix(hamburger): add link to hamburger dropdown

--- src/layouts/HamburgerList.js
+++ src/layouts/HamburgerList.js
@@ -1,14 +1,24 @@
-export default function HamburgerList({ isHambugerListShow }) {
-    const items = ['PROMOTIONS', 'BRAND', 'CATEGORIES', 'MY CART', 'LOGIN'];
+import { Link } from 'react-router-dom'
+
+export default function HamburgerList({ isHambugerListShow, onClose }) {
+    const items = [
+        { label: 'PROMOTIONS', target: '/promotions' },
+        { label: 'BRAND', target: '/brands' },
+        { label: 'CATEGORIES', target: '/categories' },
+        { label: 'MY CART', target: '/user/cart' },
+        { label: 'LOGIN', target: '/login' }
+    ];
 
     const ListItems = items.map((item, idx) => (
-        <div key={idx} className={`px-7 py-3 w-3/4 border border-1 ${idx === 0 ? '' : 'border-t-0'} border-[#D9D9D9] bg-[#767676] text-left`} >
-            {item}
-        </div>
+        <Link key={idx} to={item.target}>
+            <div key={idx} onClick={onClose} className={`px-7 py-3 w-full border border-1 ${idx === 0 ? '' : 'border-t-0'} border-[#D9D9D9] bg-[#767676] text-left`}>
+                {item.label}
+            </div>
+        </Link>
     ));
 
     return (
-        <div className="absolute w-full top-16 left-0 text-base">
+        <div className="absolute w-3/4 top-16 left-0 text-base">
             {isHambugerListShow && ListItems}
         </div>
 
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -7,13 +7,12 @@ export default function Header() {
 
     return (
         <div className="relative bg-[#767676] flex items-end text-center h-16 px-3 py-3 text-xl">
-            <div
-                className="px-1 py-0.5 cursor-pointer"
-                onClick={() => setIsHambugerListShow(!isHambugerListShow)}
-            >
-                <HamburgerIcon />
+            <div className="px-1 py-0.5 cursor-pointer" >
+                <div onClick={() => setIsHambugerListShow(!isHambugerListShow)}>
+                    <HamburgerIcon />
+                </div>
+                <HamburgerList isHambugerListShow={isHambugerListShow} onClose={() => { setIsHambugerListShow(false) }} />
             </div>
-            <HamburgerList isHambugerListShow={isHambugerListShow} />
 
             <div className="flex-1 font-rubik">
                 BAD<span className="text-[#02FECB]">MINT</span>OR
